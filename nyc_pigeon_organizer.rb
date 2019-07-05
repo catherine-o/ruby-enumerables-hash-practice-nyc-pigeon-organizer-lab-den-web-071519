8006cdad9673c4ecad5fd1cf2d212a235c690c37
Catherine O'Hara <catherine.ohara@outlook.com>
2019-07-05 00:29:05 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,7 +6,8 @@ def nyc_pigeon_organizer(data)
     info_hash.each do 
       reverse_info = info_hash.invert
       reverse_info.each do |names, details|
-        new_hash = Hash[names.collect {|name| [name, details]} ]
+        new_hash = Hash[names.collect {|name| [name, details.to_s]} ]
+        new_hash.each do |name_key, detail_str|
           new_data = {new_hash.keys => attribute}
           
    binding.pry
