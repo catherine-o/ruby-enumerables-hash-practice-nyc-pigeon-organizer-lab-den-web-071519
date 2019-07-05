cd630ba37e8232472a121c147f4346608b45f598
Catherine O'Hara <catherine.ohara@outlook.com>
2019-07-05 01:06:19 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ require "pry"
 
 def nyc_pigeon_organizer(data)
   new_hash ={}
-  data.each do |attribute, info_hash|
+  data.each_pair do |attribute, info_hash|
     info_hash.each do |detail, name_array|
       name_array.each do |name|
         if !new_hash.include?(name)
